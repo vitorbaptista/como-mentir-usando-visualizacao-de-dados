fc61a96d2c441892a461f6dc954180b33b5f5fa9
Vitor Baptista <vitor@vitorbaptista.com>
2014-02-18 18:19:39 -0300

    Commit inicial
    Versão da apresentação pra inauguração do LabHacker da Câmara Federal

---
+++ js/graficos.js
@@ -0,0 +1,53 @@
+function graficoBarras(elementId, dataPath, yMin) {
+  var margin = {top: 20, right: 20, bottom: 30, left: 40},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
+  
+  var x = d3.scale.ordinal()
+      .rangeRoundBands([0, width], .1);
+  
+  var y = d3.scale.linear()
+      .range([height, 0]);
+  
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+  
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(5);
+  
+  var svg = d3.select(elementId).append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  
+  d3.csv(dataPath, function(error, data) {
+    x.domain(data.map(function(d) { return d.letter; }));
+    y.domain([yMin, d3.max(data, function(d) { return d.frequency; })]);
+  
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+  
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+  
+    svg.selectAll(".bar")
+        .data(data)
+      .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.letter); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.frequency); })
+        .attr("height", function(d) { return height - y(d.frequency); });
+  
+  });
+}
+
+graficoBarras('#barras-fora-do-zero', 'data/data.csv', 34);
+graficoBarras('#barras-no-zero', 'data/data.csv', 0);
